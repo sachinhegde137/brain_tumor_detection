48acdb1913368cd25873054cd8dde2694abe9327
Sachin Manjunath <sachin.mhegde@gmail.com>
2024-09-03 14:19:48 +0200

    Update models.py

--- src/main.py
+++ src/main.py
@@ -1,36 +1,100 @@
 import os
 from pathlib import Path
 
-from src.data import load_data, preprocess_image
-from src.models import YoloV3
+from src.utils import DataVisualizer
+from src.models import YoloV3, ANCHORS, ANCHOR_MASKS, loss_function
+from src.data import transform_targets, tf_transform_imgs_labels
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, CSVLogger
 
 INPUT_SHAPE = (416, 416)
+BATCH_SIZE = 8
+n_classes = 4
+LEARNING_RATE = 1e-3
+EPOCHS = 1
+
+
+def create_dataset(dir_path: str) -> tf.data.Dataset:
+    """
+    Creates Tensorflow dataset object
+    :param dir_path: Path to train or validation directory
+    :return: Tensorflow dataset object
+    """
+    # Get the list of images and labels
+    images_list = []
+    label_file_list = []
+    for class_name in os.listdir(dir_path):
+        images_path = os.path.join(dir_path, class_name, 'images')
+        labels_path = os.path.join(dir_path, class_name, 'labels')
+        for image in os.listdir(images_path):
+            image_path = os.path.join(images_path, image)
+            label_file = image.replace('.jpg', '.txt')
+            label_file_path = os.path.join(labels_path, label_file)
+            if os.path.exists(label_file_path):
+                images_list.append(image_path)
+                label_file_list.append(label_file_path)
+
+    # Create TensorFlow Dataset
+    img_dataset = tf.data.Dataset.from_tensor_slices(images_list)
+    labels_dataset = tf.data.Dataset.from_tensor_slices(label_file_list)
+
+    dataset = tf.data.Dataset.zip((img_dataset, labels_dataset))
+
+    dataset = dataset.map(lambda x, y: tf_transform_imgs_labels(x, y))
+
+    return dataset
+
 
 if __name__ == '__main__':
 
     project_dir = Path(__file__).parent.parent
 
     # Paths to data directories
-    train_path = os.path.join(project_dir, "dataset/Train")
-    val_path = os.path.join(project_dir, "dataset/Val")
+    train_path = os.path.join(project_dir, "dataset\\Train")
+    val_path = os.path.join(project_dir, "dataset\\Val")
 
-    # Load training and validation data
-    #train_images, train_labels = load_data(train_path)
-    #val_images, val_labels = load_data(val_path)
+    train_dataset = create_dataset(train_path)
+    val_dataset = create_dataset(val_path)
 
-    #print(f"The size of training dataset: {len(train_images)}")
-    #print(f"The size of validation dataset: {len(val_images)}")
+    train_dataset = train_dataset.shuffle(buffer_size=512)
+    train_dataset = train_dataset.batch(batch_size=BATCH_SIZE)
+    train_dataset = train_dataset.map(
+        lambda x, y: (x, transform_targets(y, INPUT_SHAPE, ANCHORS, ANCHOR_MASKS, n_classes)))
+    train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
+    val_dataset = val_dataset.batch(batch_size=BATCH_SIZE)
+    val_dataset = val_dataset.map(
+        lambda x, y: (x, transform_targets(y, INPUT_SHAPE, ANCHORS, ANCHOR_MASKS, n_classes)))
 
-    #train_images = preprocess_image(train_images)
-    #val_images = preprocess_image(val_images)
+    train_dataset = train_dataset.skip(500)
+    val_dataset = val_dataset.skip(40)
+    print(f"The size of the training dataset: {len(list(train_dataset.as_numpy_iterator()))}")
+    print(f"The size of the validation dataset: {len(list(val_dataset.as_numpy_iterator()))}")
 
-    yolo = YoloV3(
+    model = YoloV3(
         n_classes=4,
         img_size=INPUT_SHAPE,
         max_output_size=10,
         iou_threshold=0.5,
-        confidence_threshold=0.5
+        confidence_threshold=0.5,
+        trainable=True
     )()
-    yolo.summary()
+    model.summary()
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+    loss = [loss_function(ANCHORS[mask], n_classes=n_classes) for mask in ANCHOR_MASKS]
+    model.compile(optimizer=optimizer, loss=loss, run_eagerly=True)
+
+    callbacks = [
+        ReduceLROnPlateau(verbose=1),
+        EarlyStopping(patience=10, verbose=1),
+        ModelCheckpoint('checkpoints/yolov3_{epoch}.h5', verbose=1, save_weights_only=False),
+        CSVLogger('training.log'),
+        DataVisualizer(val_dataset, result_dir='train_results')
+    ]
+
+    history = model.fit(train_dataset, epochs=EPOCHS, callbacks=callbacks, validation_data=val_dataset, verbose=True)
+
+
 
--- src/models.py
+++ src/models.py
@@ -1,15 +1,21 @@
 from typing import Union, Tuple, List, Any
+import inspect
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import BatchNormalization, Conv2D, Input, Lambda, Concatenate
 from tensorflow.keras.regularizers import l2
+from tensorflow.keras.losses import binary_crossentropy, sparse_categorical_crossentropy
 
 BATCH_NORM_DECAY = 0.9
 BATCH_NORM_EPSILON = 1e-05
 LEAKY_RELU = 0.1
-ANCHORS = [(10, 13), (16, 30), (33, 23),
-            (30, 61), (62, 45), (59, 119),
-            (116, 90), (156, 198), (373, 326)]
+ANCHORS = np.array([
+    (10, 13), (16, 30), (33, 23), (30, 61), (62, 45),
+    (59, 119), (116, 90), (156, 198), (373, 326)],
+    np.float32) / 416
+ANCHOR_MASKS = np.array([[6, 7, 8], [3, 4, 5], [0, 1, 2]])
+
 
 
 def darknet_convolution_block(
@@ -88,50 +94,31 @@ def darknet53(
     """
 
     inputs = Input([416, 416, 3])
+
     out = darknet_convolution_block(inputs, filters=32, kernel_size=3, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after layer 1: {out.shape}")
-
     out = darknet_convolution_block(out, filters=64, kernel_size=3, strides=2, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after layer 2: {out.shape}")
-
     out = darknet53_residual_block(out, filters=32, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after residual layer 1: {out.shape}")
 
     out = darknet_convolution_block(out, filters=128, kernel_size=3, strides=2, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after layer 3: {out.shape}")
-
     for _ in range(2):
         out = darknet53_residual_block(out, filters=64, trainable=trainable)
 
-    print(f"(DARKNET)The shape of the output after residual layer 2: {out.shape}")
-
     out = darknet_convolution_block(out, filters=256, kernel_size=3, strides=2, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after layer 4: {out.shape}")
-
     for _ in range(8):
         out = darknet53_residual_block(out, filters=128, trainable=trainable)
-
-    print(f"(DARKNET)The shape of the output after residual layer 3 (route1): {out.shape}")
 
     route1 = out
 
     out = darknet_convolution_block(out, filters=512, kernel_size=3, strides=2, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after layer 5: {out.shape}")
-
     for _ in range(8):
         out = darknet53_residual_block(out, filters=256, trainable=trainable)
-    print(f"(DARKNET)The shape of the output after residual layer 4 (route2): {out.shape}")
 
     route2 = out
 
     out = darknet_convolution_block(out, filters=1024, kernel_size=3, strides=2, trainable=trainable)
-
-    print(f"(DARKNET)The shape of the output after layer 6: {out.shape}")
-
     for _ in range(4):
         out = darknet53_residual_block(out, filters=512, trainable=trainable)
 
-    print(f"(DARKNET)The shape of the output after residual layer 5 (output): {out.shape}")
     darknet_model = tf.keras.Model(inputs, (route1, route2, out), name=name)
     return darknet_model
 
@@ -152,27 +139,15 @@ def yolo_convolution_block(
     :return: 2 Output tensors
     """
     inputs = Input(input_tensor.shape[1:])
-    print(f"(YOLOCONV)The shape of the input: {input_tensor.shape}")
     out = darknet_convolution_block(inputs, filters=filters, kernel_size=1, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 1: {out.shape}")
-
     out = darknet_convolution_block(out, filters=2 * filters, kernel_size=3, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 2: {out.shape}")
-
     out = darknet_convolution_block(out, filters=filters, kernel_size=1, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 3: {out.shape}")
-
     out = darknet_convolution_block(out, filters=2 * filters, kernel_size=3, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 4: {out.shape}")
-
     out = darknet_convolution_block(out, filters=filters, kernel_size=1, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 5(route): {out.shape}")
 
     route = out
 
     out = darknet_convolution_block(out, filters=2 * filters, kernel_size=3, trainable=trainable)
-    print(f"(YOLOCONV)The shape of the output after layer 6(output): {out.shape}")
-
     yolo_conv_block = tf.keras.Model(inputs, (route, out), name=name)
     return yolo_conv_block
 
@@ -183,7 +158,7 @@ def yolo_output_layer(
         filters: int,
         trainable: bool,
         n_classes: int,
-        anchors: List[Tuple[int, int]],
+        anchors: int,
 ) -> Model:
     """
     Creates Yolo final detection layer.
@@ -197,41 +172,36 @@ def yolo_output_layer(
     :param anchors: A list of anchor sizes.
     :return: Output tensor
     """
-    n_anchors = len(anchors)
     inputs = Input(input_tensor.shape[1:])
     out = darknet_convolution_block(inputs, filters=filters, kernel_size=3, trainable=trainable)
-    print(f"(YOLOOUT)The shape of the output after layer 1: {out.shape}")
     out = darknet_convolution_block(
-        out, filters=n_anchors * (n_classes + 5), kernel_size=1, use_batch_norm=False, trainable=trainable
+        out, filters=anchors * (n_classes + 5), kernel_size=1, use_batch_norm=False, trainable=trainable
     )
-    print(f"(YOLOOUT)The shape of the output after layer 2: {out.shape}")
     out = Lambda(
         lambda out: tf.reshape(
-            out, (-1, tf.shape(out)[1], tf.shape(out)[2], n_anchors, n_classes + 5)
+            out, (-1, tf.shape(out)[1], tf.shape(out)[2], anchors, n_classes + 5)
         )
     )(out)
-    print(f"(YOLOOUT)The shape of the output after layer 3: {out.shape}")
     return tf.keras.Model(inputs, out, name=name)
 
 
 def build_boxes(
         inputs: tf.Tensor,
-        anchors: List[Tuple[int, int]],
+        anchors: np.ndarray,
         n_classes: int
 ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
     """
     Computes top left and bottom right points of the boxes.
-    :param anchors: A list of anchor sizes.
+    :param anchors: A list of anchors
     :param inputs: Input tensor
     :param n_classes: Number of classes
     :return: Output tensor
     """
-    n_anchors = len(anchors)
 
     # inputs: (batch_size, grid, grid, anchors, (x, y, w, h, obj, ...classes))
     grid_size = tf.shape(inputs)[1:3][::-1]
     grid_y, grid_x = tf.shape(inputs)[1], tf.shape(inputs)[2]
-
+    stack = inspect.stack()
     box_xy, box_wh, objectness, class_probs = tf.split(inputs, (2, 2, 1, n_classes), axis=-1)
     box_xy = tf.sigmoid(box_xy)
 
@@ -239,12 +209,11 @@ def build_boxes(
     class_probs = tf.nn.softmax(class_probs)
     pred_box = tf.concat((box_xy, box_wh), axis=-1)  # original xywh for loss
 
-    # !!! grid[x][y] == (y, x)
     grid = tf.meshgrid(tf.range(grid_x), tf.range(grid_y))
     grid = tf.expand_dims(tf.stack(grid, axis=-1), axis=2)  # [gx, gy, 1, 2]
 
     box_xy = (box_xy + tf.cast(grid, tf.float32)) / tf.cast(grid_size, tf.float32)
-    box_wh = tf.exp(box_wh) * n_anchors
+    box_wh = tf.math.exp(box_wh) * anchors
 
     box_x1y1 = box_xy - box_wh / 2
     box_x2y2 = box_xy + box_wh / 2
@@ -291,13 +260,103 @@ def non_max_suppression(
     return nms_boxes, nms_scores, nms_classes, nms_valid_detections
 
 
+def broadcast_iou(box_1: tf.Tensor, box_2: tf.Tensor) -> Any:
+    # box_1: (..., (x1, y1, x2, y2))
+    # box_2: (N, (x1, y1, x2, y2))
+
+    # broadcast boxes
+    box_1 = tf.expand_dims(box_1, -2)
+    box_2 = tf.expand_dims(box_2, 0)
+    # new_shape: (..., N, (x1, y1, x2, y2))
+    new_shape = tf.broadcast_dynamic_shape(tf.shape(box_1), tf.shape(box_2))
+    box_1 = tf.broadcast_to(box_1, new_shape)
+    box_2 = tf.broadcast_to(box_2, new_shape)
+
+    int_w = tf.maximum(tf.minimum(box_1[..., 2], box_2[..., 2]) - tf.maximum(box_1[..., 0], box_2[..., 0]), 0)
+    int_h = tf.maximum(tf.minimum(box_1[..., 3], box_2[..., 3]) - tf.maximum(box_1[..., 1], box_2[..., 1]), 0)
+    int_area = int_w * int_h
+    box_1_area = (box_1[..., 2] - box_1[..., 0]) * (box_1[..., 3] - box_1[..., 1])
+    box_2_area = (box_2[..., 2] - box_2[..., 0]) * (box_2[..., 3] - box_2[..., 1])
+    return int_area / (box_1_area + box_2_area - int_area)
+
+
+def loss_function(
+        anchors: np.ndarray,
+        n_classes: int,
+        ignore_thresh: float = 0.5
+) -> Any:
+    """
+    Loss function to train the model
+
+    :param anchors: A list of anchors
+    :param n_classes: Number of classes
+    :param ignore_thresh: Ignore threshold
+    :return: yolo loss function
+    """
+
+    def yolo_loss(y_true, y_pred):
+        # 1. transform all pred outputs
+        # y_pred: (batch_size, grid, grid, anchors, (x, y, w, h, obj, ...cls))
+        pred_box, pred_obj, pred_class, pred_xywh = build_boxes(
+            inputs=y_pred,
+            anchors=anchors,
+            n_classes=n_classes
+        )
+        pred_xy = pred_xywh[..., 0:2]
+        pred_wh = pred_xywh[..., 2:4]
+        # 2. transform all true outputs
+        # y_true: (batch_size, grid, grid, anchors, (x1, y1, x2, y2, obj, cls))
+        true_box, true_obj, true_class_idx = tf.split(
+            y_true, (4, 1, 1), axis=-1)
+        true_xy = (true_box[..., 0:2] + true_box[..., 2:4]) / 2
+        true_wh = true_box[..., 2:4] - true_box[..., 0:2]
+        # give higher weights to small boxes
+        box_loss_scale = 2 - true_wh[..., 0] * true_wh[..., 1]
+        # 3. inverting the pred box equations
+        grid_size = tf.shape(y_true)[1]
+        grid = tf.meshgrid(tf.range(grid_size), tf.range(grid_size))
+        grid = tf.expand_dims(tf.stack(grid, axis=-1), axis=2)
+        true_xy = true_xy * tf.cast(grid_size, tf.float32) - \
+                  tf.cast(grid, tf.float32)
+        true_wh = tf.math.log(true_wh / anchors)
+        true_wh = tf.where(tf.math.is_inf(true_wh), tf.zeros_like(true_wh), true_wh)
+        # 4. calculate all masks
+        obj_mask = tf.squeeze(true_obj, -1)
+        # ignore false positive when iou is over threshold
+        true_box_flat = tf.boolean_mask(true_box, tf.cast(obj_mask, tf.bool))
+        best_iou = tf.reduce_max(broadcast_iou(
+            pred_box, true_box_flat), axis=-1)
+        ignore_mask = tf.cast(best_iou < ignore_thresh, tf.float32)
+        # 5. calculate all losses
+        xy_loss = obj_mask * box_loss_scale * \
+                  tf.reduce_sum(tf.square(true_xy - pred_xy), axis=-1)
+        wh_loss = obj_mask * box_loss_scale * \
+                  tf.reduce_sum(tf.square(true_wh - pred_wh), axis=-1)
+        obj_loss = binary_crossentropy(true_obj, pred_obj)
+        obj_loss = obj_mask * obj_loss + \
+                   (1 - obj_mask) * ignore_mask * obj_loss
+        # Could also use binary_crossentropy instead
+        class_loss = obj_mask * sparse_categorical_crossentropy(
+            true_class_idx, pred_class)
+        # 6. sum over (batch, gridx, gridy, anchors) => (batch, 1)
+        xy_loss = tf.reduce_sum(xy_loss, axis=(1, 2, 3))
+        wh_loss = tf.reduce_sum(wh_loss, axis=(1, 2, 3))
+        obj_loss = tf.reduce_sum(obj_loss, axis=(1, 2, 3))
+        class_loss = tf.reduce_sum(class_loss, axis=(1, 2, 3))
+        return xy_loss + wh_loss + obj_loss + class_loss
+
+    return yolo_loss
+
+
+
+
 class YoloV3:
     """
     Yolo v3 model class.
     """
 
     def __init__(self, n_classes, img_size, max_output_size, iou_threshold,
-                 confidence_threshold):
+                 confidence_threshold, trainable=False):
         """
         Creates the model.
 
@@ -314,8 +373,9 @@ class YoloV3:
         self.max_output_size = max_output_size
         self.iou_threshold = iou_threshold
         self.confidence_threshold = confidence_threshold
+        self.trainable = trainable
 
-    def __call__(self, trainable=False):
+    def __call__(self):
         """
         Add operations to detect boxes for a batch of input images.
 
@@ -326,83 +386,68 @@ class YoloV3:
 
         route1, route2, out = darknet53(
             name="Darknet_Block",
-            trainable=trainable,
+            trainable=self.trainable,
         )(inputs)
-        print(f"The shape of the output after darknet block: {out.shape}")
         route, out = yolo_convolution_block(
             name="Yolo_Conv_0",
             input_tensor=out,
             filters=512,
-            trainable=trainable
+            trainable=self.trainable
         )(out)
-        print(f"The shape of the output after yolo convolution block: {out.shape}")
         detect1 = yolo_output_layer(
             name="Yolo_Output_0",
             input_tensor=out,
             filters=512,
-            trainable=trainable,
+            trainable=self.trainable,
             n_classes=self.n_classes,
-            anchors=ANCHORS[6:9]
+            anchors=len(ANCHOR_MASKS[0]),
         )(out)
-        print(f"The shape of the output after yolo output layer 0 (detect 1): {detect1.shape}")
 
-        out = darknet_convolution_block(out, filters=256, kernel_size=1, trainable=trainable)
-
+        out = darknet_convolution_block(out, filters=256, kernel_size=1, trainable=self.trainable)
         out = tf.keras.layers.UpSampling2D(size=2, interpolation="nearest")(out)
-        print(f"The shape of the output after upsampling layer: {out.shape}")
         out = Concatenate(axis=3)([out, route2])
 
         route, out = yolo_convolution_block(
             name="Yolo_Conv_1",
             input_tensor=out,
             filters=256,
-            trainable=trainable
+            trainable=self.trainable
         )(out)
         detect2 = yolo_output_layer(
             name="Yolo_Output_1",
             input_tensor=out,
             filters=256,
-            trainable=trainable,
+            trainable=self.trainable,
             n_classes=self.n_classes,
-            anchors=ANCHORS[3:6]
+            anchors=len(ANCHOR_MASKS[1]),
         )(out)
-        print(f"The shape of the output after yolo output layer 1 (detect 2): {detect2.shape}")
 
-        out = darknet_convolution_block(out, filters=128, kernel_size=1, trainable=trainable)
-
+        out = darknet_convolution_block(out, filters=128, kernel_size=1, trainable=self.trainable)
         out = tf.keras.layers.UpSampling2D(size=2, interpolation="nearest")(out)
-        print(f"The shape of the output after upsampling layer 2: {out.shape}")
         out = Concatenate(axis=3)([out, route1])
 
         route, out = yolo_convolution_block(
             name="Yolo_Conv_2",
             input_tensor=out,
             filters=128,
-            trainable=trainable
+            trainable=self.trainable
         )(out)
         detect3 = yolo_output_layer(
             name="Yolo_Output_2",
             input_tensor=out,
             filters=128,
-            trainable=trainable,
+            trainable=self.trainable,
             n_classes=self.n_classes,
-            anchors=ANCHORS[0:3]
+            anchors=len(ANCHOR_MASKS[2]),
         )(out)
-        print(f"The shape of the output after yolo output layer 2 (detect 3): {detect3.shape}")
 
-        if trainable:
+        if self.trainable:
             return Model(inputs, (detect1, detect2, detect3), name='Yolov3')
 
-        print(f"The shape of the 3 output layers: Layer 82: {detect1.shape}, "
-              f"Layer 94: {detect2.shape}, Layer 106: {detect3.shape}")
+        boxes_0 = Lambda(lambda x: build_boxes(x, ANCHORS[ANCHOR_MASKS[0]], self.n_classes), name='yolo_boxes_0')(detect1)
+        boxes_1 = Lambda(lambda x: build_boxes(x, ANCHORS[ANCHOR_MASKS[1]], self.n_classes), name='yolo_boxes_1')(detect2)
+        boxes_2 = Lambda(lambda x: build_boxes(x, ANCHORS[ANCHOR_MASKS[2]], self.n_classes), name='yolo_boxes_2')(detect3)
 
-        boxes_0 = Lambda(lambda x: build_boxes(x, ANCHORS[6:9], self.n_classes), name='yolo_boxes_0')(detect1)
-        boxes_1 = Lambda(lambda x: build_boxes(x, ANCHORS[3:6], self.n_classes), name='yolo_boxes_1')(detect2)
-        boxes_2 = Lambda(lambda x: build_boxes(x, ANCHORS[0:3], self.n_classes), name='yolo_boxes_2')(detect3)
-
-        print(f"The shape of the output before NMS: BBOX: {boxes_0[0].shape}"
-              f" OBJECTNESS: {boxes_0[1].shape} CLASS_PROB: {boxes_0[2].shape}"
-              f" PRED: {boxes_0[3].shape}")
         outputs = Lambda(
             lambda x: non_max_suppression(
                 x, max_output_size=100, iou_threshold=0.5, confidence_threshold=0.5
@@ -410,11 +455,9 @@ class YoloV3:
             name='yolo_nms'
         )((boxes_0[:3], boxes_1[:3], boxes_2[:3]))
 
-        print(f"The shape of the output after NMS: BBOX: {outputs[0].shape}"
-              f" OBJECTNESS: {outputs[1].shape} CLASS_PROB: {outputs[2].shape}"
-              f" PRED: {outputs[3].shape}")
-
         return Model(inputs, outputs, name='Yolov3')
+
+
 
 
 
